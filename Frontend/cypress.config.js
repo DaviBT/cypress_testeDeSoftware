5caf140be4b979424ce8ca97d6638e8a994d3279
Davi <davi.cesarbt@gmail.com>
2024-08-22 09:53:03 -0300

    testes da pagina de formularioDeLogin

--- Frontend/cypress.config.js
+++ Frontend/cypress.config.js
@@ -5,8 +5,3 @@ module.exports = {
     },
   },
 };
-
-module.exports = defineConfig({
-  e2e: {
-  baseUrl: 'http://localhost:3000' }, 
-});
--- Frontend/cypress/e2e/paginas.cy.js
+++
@@ -1,4 +0,0 @@
-describe('Testando múltiplas páginas', () => {
-    it('Deve conseguir acessar a página de cartões'), () => {
-    cy.visit('http://localhost:3000')
-    }})
