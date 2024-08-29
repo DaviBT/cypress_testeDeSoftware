e7a4763a9eee0d27ef77236fa26ad46847639910
Davi <davi.cesarbt@gmail.com>
2024-08-29 09:37:42 -0300

    testes diversos utilizando o comando viewport para testar a altura e largura da janela de exibição, além do teste em modo paisagem

--- Frontend/cypress/e2e/dispositivosMoveis.cy.js
+++ Frontend/cypress/e2e/dispositivosMoveis.cy.js
@@ -21,6 +21,17 @@ describe('Menu de navegação', () => {
       beforeEach(() => {
         /* Roda os testes como se fossem em um monitor de 720p de resolução */
         cy.viewport(1280, 720)
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()			
+
+        cy.location('pathname').should('eq','/home/investimentos')		
       })
 
       /* seu teste aqui */
@@ -31,9 +42,41 @@ describe('Menu de navegação', () => {
       beforeEach(() => {
         /* roda os testes como se fossem em um dispositivo com a resolução de um iphone-5 */
         cy.viewport('iphone-5')
+        cy.viewport(1280, 720)
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()			
+
+        cy.location('pathname').should('eq','/home/investimentos')	
       })
 
       /* seu teste aqui */
       
+    })
+
+    context('Testes em modo paisagem', () => {
+        beforeEach(() => {
+          /* roda os testes no modo paisagem */
+          cy.viewport('iphone-6', 'landscape')
+          cy.viewport('iphone-5')
+          cy.viewport(1280, 720)
+          cy.getByData('botao-login').click()
+          cy.getByData('email-input').type('neilton@alura.com')
+          cy.getByData('senha-input').type('123456')
+          cy.getByData('botao-enviar').click()
+          
+          cy.location('pathname').should('eq','/home')
+  
+          cy.getByData('menu-burguer').click()
+          cy.getByData('menu-lateral').find('a').eq(3).click()			
+  
+          cy.location('pathname').should('eq','/home/investimentos')	
+        })
     })
   })
